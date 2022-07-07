c8a1111ccb6c42fd85b566f059d6130391ecc04a
Timo Ramsdorf <t_rams01@wwu.de>
2022-07-08 00:32:47 +0200

    intercept responses

--- hook-in/src/main/java/dataminimization/DataMinimizerInterceptor.java
+++ hook-in/src/main/java/dataminimization/DataMinimizerInterceptor.java
@@ -3,11 +3,8 @@ package dataminimization;
 import accesscontrol.Authorization;
 import accesscontrol.ConfigParser;
 import com.google.protobuf.Message;
+import io.grpc.*;
 import io.grpc.ForwardingServerCallListener.SimpleForwardingServerCallListener;
-import io.grpc.Metadata;
-import io.grpc.ServerCall;
-import io.grpc.ServerCallHandler;
-import io.grpc.ServerInterceptor;
 
 public class DataMinimizerInterceptor implements ServerInterceptor {
 
@@ -31,8 +28,16 @@ public class DataMinimizerInterceptor implements ServerInterceptor {
                 // noop
             };
         }
-        ServerCall.Listener listener = serverCallHandler.startCall(serverCall, metadata);
-        return new SimpleForwardingServerCallListener<ReqT>(listener) {
+        ServerCall.Listener<ReqT> listener = serverCallHandler.startCall(new ForwardingServerCall.SimpleForwardingServerCall<>(serverCall) {
+            @Override
+            public void sendMessage(RespT message) {
+                if (message instanceof Message) {
+                    message = (RespT) minimizer.minimize((Message) message, authorization.getPurposeOrNull());
+                }
+                super.sendMessage(message);
+            }
+        }, metadata);
+        return new SimpleForwardingServerCallListener<>(listener) {
             @Override
             public void onMessage(ReqT req) {
                 if (req instanceof Message) {
