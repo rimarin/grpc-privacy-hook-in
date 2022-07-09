2f1eff2bde98f364659cfcbde41a541ab12f72ba
Riccardo Marin <riccardomarin23@gmail.com>
2022-07-09 14:30:01 +0200

    benchmarking jmeter test cases, starting script for benchmarking

---
+++ benchmark/launch_benchmarks.sh
@@ -0,0 +1,6 @@
+
+
+# apache-jmeter-5.5/bin/jmeter -n -t without_component.jmx -l without_component_results.jtl
+apache-jmeter-5.5/bin/jmeter -n -t without_component.jmx -l without_component_results.csv
+#java -jar JMeterPluginsCMD.jar --generate-csv with-component-results.csv --input-jtl without_component_results.jtl --plugin-type AggregateReport
+# apache-jmeter-5.5/bin/jmeter -n -t with_component.jmx -l with_component_results.csv
