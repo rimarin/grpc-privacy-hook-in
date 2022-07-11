1edff86d7b415fdb7398c052b2984c3d5bd15c9e
Riccardo Marin <riccardomarin23@gmail.com>
2022-07-11 14:30:04 +0200

    key generator key export to specific folder

--- hook-in/src/main/resources/privateKeys/keys_generator.sh
+++ hook-in/src/main/resources/privateKeys/keys_generator.sh
@@ -6,7 +6,8 @@ for i in "${!arr[@]}"; do
   # Generate a 2048-bit RSA private key
   openssl genrsa -out private_key_"${arr[i]}".pem 2048
   # Convert private Key to PKCS#8 format (readable by Java)
-  openssl pkcs8 -topk8 -inform PEM -outform DER -in private_key_"${arr[i]}".pem -out private_key_"${arr[i]}".der -nocrypt
+  # openssl pkcs8 -topk8 -inform PEM -outform DER -in private_key_"${arr[i]}".pem -out private_key_"${arr[i]}".der -nocrypt
+  openssl pkcs8 -topk8 -inform PEM -outform DER -in private_key_"${arr[i]}".pem -out ../../../../../delivery/src/main/resources/privateKeys/private_key_"${arr[i]}".der -nocrypt
   # Output public key portion in DER format (readable by Java)
   openssl rsa -in private_key_"${arr[i]}".pem -pubout -outform DER -out ../../../../../hook-in/src/main/resources/publicKeys/public_key_"${arr[i]}".der
 done
