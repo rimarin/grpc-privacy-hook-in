e0020c9ce259af06f0c686d7991e977e5b4de012
Timo Ramsdorf <t_rams01@wwu.de>
2022-07-13 17:15:45 +0200

    configuration cache -> speedup 2

--- hook-in/src/main/java/accesscontrol/ConfigParser.java
+++ hook-in/src/main/java/accesscontrol/ConfigParser.java
@@ -2,21 +2,43 @@ package accesscontrol;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.*;
 
 public class ConfigParser {
 
     private JsonNode purposesConfig;
+    private Map<String, List<String>> allowedClients = new HashMap<>();
+    private Map<String, List<String>> allowedMethods = new HashMap<>();
 
     public ConfigParser(String fileName) {
         ObjectMapper mapper = new ObjectMapper();
         try {
             String content = new String(Files.readAllBytes(Paths.get(fileName)));
             purposesConfig = mapper.readTree(content);
+            Iterator<Map.Entry<String, JsonNode>> purposes = purposesConfig.at("/purposes").fields();
+            while (purposes.hasNext()) {
+                Map.Entry<String, JsonNode> purpose = purposes.next();
+                allowedClients.put(purpose.getKey(), new LinkedList<>());
+                JsonNode elements = purpose.getValue().at("/allowed_clients");
+                if (elements != null) {
+                    Iterator<JsonNode> clientStrings = elements.elements();
+                    while (clientStrings.hasNext()) {
+                        allowedClients.get(purpose.getKey()).add(clientStrings.next().asText());
+                    }
+                }
+                allowedMethods.put(purpose.getKey(), new LinkedList<>());
+                elements = purpose.getValue().at("/allowed_methods");
+                if (elements != null) {
+                    Iterator<JsonNode> methodStrings = elements.elements();
+                    while (methodStrings.hasNext()) {
+                        allowedMethods.get(purpose.getKey()).add(methodStrings.next().asText());
+                    }
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -35,25 +57,11 @@ public class ConfigParser {
     }
 
     public boolean isClientRejectedForPurpose(String purpose, String clientId) {
-        return isRejectedForPurpose(purpose, clientId, "allowed_clients");
+        return !allowedClients.getOrDefault(purpose, new LinkedList<>()).contains(clientId);
     }
 
     public boolean isMethodRejectedForPurpose(String purpose, String methodName) {
-        return isRejectedForPurpose(purpose, methodName, "allowed_methods");
-    }
-
-    private boolean isRejectedForPurpose(String purpose, String givenValue, String jsonKeyword) {
-        // TODO: caching json file
-        // TODO: nullPointerException when purpose name is not found
-        ArrayNode allowedValues = (ArrayNode) purposesConfig.get("purposes").get(purpose).get(jsonKeyword);
-        if (allowedValues.isArray()) {
-            for (JsonNode allowedValue : allowedValues) {
-                if (givenValue.equals(allowedValue.asText())) {
-                    return false;
-                }
-            }
-        }
-        return true;
+        return !allowedMethods.getOrDefault(purpose, new LinkedList<>()).contains(methodName);
     }
 
 }
